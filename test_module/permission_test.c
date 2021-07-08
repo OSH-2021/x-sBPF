11973ecc51c4248aae95124f8460e3a7e23ac535
lht <kk2048@mail.ustc.edu.cn>
2021-07-08 00:15:49 -0700

    add sBPF type permission

---
+++ test_module/permission_test.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    
+   char test_space[1024];
+   
+     
+    
+    char buffer[1024];
+    FILE * fp;
+
+    
+    
+    printf("swap_space %lu\n",(unsigned long)test_space);
+    printf("这个程序的PID为: %d\n", getpid());
+    getchar();  
+    
+    fp = fopen ("./../file.txt", "w");
+    if(!fp){
+	return 0;
+    }
+    fprintf(fp, "%s\n", "test_message_11111");
+    fclose(fp);
+    
+    fp = fopen ("./../../file.txt", "w");
+    if(!fp){
+	return 0;
+    }
+    fprintf(fp, "%s\n", "test_message_11111");
+    fclose(fp);
+    
+    fp = fopen ("./../../../file.txt", "w");
+    if(!fp){
+	return 0;
+    }
+    fprintf(fp, "%s\n", "test_message_11111");
+    fclose(fp);
+    
+    fp = fopen ("./../../../../file.txt", "w");
+    if(!fp){
+	return 0;
+    }
+    fprintf(fp, "%s\n", "test_message_11111");
+    fclose(fp);
+    
+    return 0;
+}
+
