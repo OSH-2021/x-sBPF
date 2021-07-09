b1432375be67ba2a0cd3c00c611f3414912cc21c
lht <kk2048@mail.ustc.edu.cn>
2021-07-08 17:59:35 -0700

    update perf_test

---
+++ test_module/perf_test.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+main()
+{
+    
+   char test_space[1024];
+   
+     
+    
+    
+    
+    /*
+    printf("swap_space %lu\n",(unsigned long)test_space);
+    printf("这个程序的PID为: %d\n", getpid());
+    getchar();  
+    */
+    printf("这个程序的PID为: %d\n", getpid());
+    getchar();  
+    
+    int start=clock();
+    
+    char buffer[1024];
+    FILE * fp;
+    
+    for(int i=0;i<=10000;i++){
+	    fp = fopen ("file.txt", "w");
+	    fprintf(fp, "%s\n", "test_message_11111");
+	    fclose(fp);
+	    
+	    fp = fopen ("file.txt", "r");
+	    fscanf(fp,"%s",buffer);
+	    //printf("%s\n",buffer);
+	    fclose(fp);
+    }
+    printf("time:%d\n",clock()-start);
+    /*
+    fp = fopen ("file.txt", "w");
+    fprintf(fp, "%s\n", "test_message_222222");
+    fclose(fp);
+    
+    fp = fopen ("file.txt", "r");
+    fscanf(fp,"%s",buffer);
+    printf("%s\n",buffer);
+    fclose(fp);
+    */
+}
+
